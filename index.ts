c885ec843234054a7e16b00a04b92844ac3a8f43
LendyAbel <lendyabel93@gmail.com>
2025-10-07 15:55:50 +0200

    9.4 Express

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (_req, res)=>{
+    res.send('Hello Full Stack!')
+})
+
+const PORT = 3003
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
