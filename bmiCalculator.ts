da26d90f2ec1cef1266bf708a1c92db52887cdd8
LendyAbel <lendyabel93@gmail.com>
2025-10-08 10:36:02 +0200

    9.5 WebBMI

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -7,14 +7,17 @@ const calculateBmi = (height: number, weight: number): string => {
   return getBMICategory(bmi)
 }
 
-
-try {
-  const { height, weight } = parseArgumentsBmi(process.argv)
-  console.log(calculateBmi(height, weight))
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happend.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArgumentsBmi(process.argv)
+    console.log(calculateBmi(height, weight))
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happend.'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message
+    }
+    console.log(errorMessage)
   }
-  console.log(errorMessage)
 }
+
+export default calculateBmi
