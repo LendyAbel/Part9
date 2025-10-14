ea51026532e50eeb963e261d11bc557314b51f3d
LendyAbel <lendyabel93@gmail.com>
2025-10-14 14:13:06 +0200

    9.6 Eslint

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,23 +1,23 @@
-import { getBMICategory } from './helper'
-import { parseArgumentsBmi } from './helper'
+import { getBMICategory } from './helper';
+import { parseArgumentsBmi } from './helper';
 
 const calculateBmi = (height: number, weight: number): string => {
-  const heihtInMeters = height / 100
-  const bmi = weight / (heihtInMeters ^ 2)
-  return getBMICategory(bmi)
-}
+  const heihtInMeters = height / 100;
+  const bmi = weight / (heihtInMeters ^ 2);
+  return getBMICategory(bmi);
+};
 
 if (require.main === module) {
   try {
-    const { height, weight } = parseArgumentsBmi(process.argv)
-    console.log(calculateBmi(height, weight))
+    const { height, weight } = parseArgumentsBmi(process.argv);
+    console.log(calculateBmi(height, weight));
   } catch (error: unknown) {
-    let errorMessage = 'Something bad happend.'
+    let errorMessage = 'Something bad happend.';
     if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message
+      errorMessage += ' Error: ' + error.message;
     }
-    console.log(errorMessage)
+    console.log(errorMessage);
   }
 }
 
-export default calculateBmi
+export default calculateBmi;
--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-import express from 'express'
-import calculateBmi from './bmiCalculator'
+import express from 'express';
+import calculateBmi from './bmiCalculator';
 
-const app = express()
+const app = express();
 
 app.get('/bmi', (req, res) => {
-  const height = Number(req.query.height)
-  const weight = Number(req.query.weight)
-  if(isNaN(height) || isNaN(weight) || !height || !weight) {
-   return res.status(400).json({error: 'malformatted parameters'})
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  if (isNaN(height) || isNaN(weight) || !height || !weight) {
+    return res.status(400).json({ error: 'malformatted parameters' });
   }
-  const bmi = calculateBmi(height, weight)
-  return res.json({ weight, height, bmi })
-})
+  const bmi = calculateBmi(height, weight);
+  return res.json({ weight, height, bmi });
+});
 
-const PORT = 3003
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
