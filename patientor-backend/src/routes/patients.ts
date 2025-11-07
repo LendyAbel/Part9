dae0d1145fc7e2b2d0fb1fb0da5b519ab1b1acec
LendyAbel <lendyabel93@gmail.com>
2025-11-07 13:56:06 +0100

    9.21: Patientor, step1

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,12 +1,17 @@
 import patientsServices from '../services/patientesServices';
 import express, { Response } from 'express';
-import { PatientNoSsn } from '../types';
+import { Patient, PatientNoSsn } from '../types';
 import { toNewPatientEntry } from '../utils';
 
 const router = express.Router();
 
 router.get('/', (_req, res: Response<PatientNoSsn[]>) => {
   res.send(patientsServices.getPatientesNoSsn());
+});
+
+router.get('/:id', (req, res: Response<Patient>)=>{
+  const id =  req.params.id;
+  res.send(patientsServices.getPatientById(id));
 });
 
 router.post('/', (req, res) => {
